719d77b6c846ca38944d55fdf3ef5cc2b42a71f5
岩元隆明 <iwamoto.takaaki@gmail.com>
2014-06-26 01:39:15 +0900

    routes.rb get '/' add.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleApp::Application.routes.draw do
   root 'static_pages#home'
+  match '/', to: 'static_pages#home', via:'get'
   match '/signup', to: 'users#new', via:'get'
   match '/help', to: 'static_pages#help', via:'get'
   match '/about', to: 'static_pages#about', via:'get'
